1aedec7f7fc158d57fe67961e42f6e7e6d2b34e5
Paolo Lucas <lucaspaolo002@gmail.com>
2023-09-13 18:18:12 -0300

    Refactor de constructor

--- src/ar/edu/unq/po2/tp3/punto.java
+++ src/ar/edu/unq/po2/tp3/punto.java
@@ -2,4 +2,31 @@ package ar.edu.unq.po2.tp3;
 
 public class punto {
 
+	private float x;
+	private float y;
+	public punto(float x, float y) {
+		super();
+		this.setXY (x, y);
+	}
+	
+	public void setXY (float x, float y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	
+		
 }
